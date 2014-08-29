866013cb4a6891e3ee12e6d0c8e77aac41748fb9
mochalov.serge <mochalov.serge@gmail.com>
2014-08-29 14:41:25 +0700

    закончил основную работу по пролог-программе, остается сделать предикат по заданию в кр

--- prolog_kr/file1.pl
+++ prolog_kr/file1.pl
@@ -1,36 +1,35 @@
 % Автор:
 % Дата: 28.08.2014
 :-dynamic автор/4.
-goal:-загрузить_базу_данных,
+
+goal:-загрузка,
       показать_меню.
 
+загрузка:-consult('C:\\Users\\serge\\Институт\\source\\University\\prolog_kr\\autors.txt'),
+          told.
 показать_меню:-repeat,
-         write('1. создать базу данных'), nl,
-         write('2. просмотр базы данных'), nl,
-         write('3. добавление записи'), nl,
-         write('4. удаление записи'), nl,
-         write('5. запрос к базе данных по заданию'), nl,
-         write('6. выход'), nl, nl,
-         write('введите номер: '),
-         read(X),
-         X < 6, process(X),
-         X = 6,!.
-         
-process(6).
-process(1):-создать_базу_данных.
-process(2):-просмотр_базы_данных.
-process(3):-запись_в_базу.
-process(4):-удалить_запись_из_базы.
-process(5).
+               write('1. просмотр базы данных'), nl,
+               write('2. добавление записи'), nl,
+               write('3. удаление записи'), nl,
+               write('4. запрос к базе данных по заданию'), nl,
+               write('0. выход'), nl, nl,
+               write('введите номер: '),
+               read(X),
+               process(X),!.
+               
+process(0).
+process(1):-просмотр_базы_данных.
+process(2):-добавление_записи.
+process(3):-удалить_запись.
+process(4):-запрос_к_базе.
+process(_):-fail.
 
-создать_базу_данных:-write('формирование базы'), nl,
-                     repeat,
-                     write('новая запись? y/n'), nl,
-                     read(A),
-                     ответ(A),
-                     tell('D:\\Институт\\source\\University\\prolog_kr\\autors.txt'),
-                     listing(автор/4),
-                     told.
+добавление_записи:-write('формирование базы'), nl,
+                   repeat,
+                   write('новая запись? y/n'), nl,
+                   read(A),
+                   ответ(A),!,fail.
+
 ответ(n).
 ответ(y):-запись_в_базу,
           fail.
@@ -44,22 +43,21 @@ process(5).
                read(Producer),
                write('год издания:'),
                read(Year),
-               assertz(автор(AuthorName, BookName, Producer, Year)).
-
-загрузить_базу_данных:-consult('D:\\Институт\\source\\University\\prolog_kr\\autors.txt'),
-                       told.
+               assertz(автор(AuthorName, BookName, Producer, Year)),
+               сохранить_файл.
 
 просмотр_базы_данных:-write('Просмотр базы данных'), nl,
-                      listing(автор/4).
+                      listing(автор/4),
+                      fail.
 
-удалить_запись_из_базы:-write('Удалить по имени: '),
-                        read(AuthorName),
-                        автор(AuthorName, _, _, _),
-                        write(AuthorName), write(' - удален.'), nl,
-                        retract(автор(AuthorName, _, _, _)),
-                        fail.
+удалить_запись:-write('Удалить по имени: '),
+                read(AuthorName),
+                автор(AuthorName, _, _, _),
+                write(AuthorName), write(' - удален.'), nl, nl,
+                retract(автор(AuthorName, _, _, _)),
+                сохранить_файл,
+                fail.
 
-сохранить_базу_данных:-tell('D:\\Институт\\source\\University\\prolog_kr\\autors.txt'),
-                       listing(автор/4),
-                       told.
-
+сохранить_файл:-tell('C:\\Users\\serge\\Институт\\source\\University\\prolog_kr\\autors.txt'),
+                listing(автор/4),
+                told.
